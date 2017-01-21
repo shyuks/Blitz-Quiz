1d78132d6a7ea504d8e05fb23ca17b6d31fa6c4f
Bong Chu Chon <bongchon@Bongs-MacBook-Pro.local>
2017-01-21 11:58:31 -0800

    [setup]changes to gitignore, setting up server, and routes

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+let port = 9000;
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json())
+
+
+app.listen(port, function () {
+	console.log("listening on port " + port);
+});
