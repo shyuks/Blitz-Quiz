c4482e9851397a46a782cf503a2b216ac0ce40b3
EJMason <eliotjunior@gmail.com>
2017-01-21 10:42:52 -0800

    [setup] all files for barebones scaffolding

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
