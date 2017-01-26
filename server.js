1178efea1d3a48fe02591f848d41912cdbed69da
EJMason <eliotjunior@gmail.com>
2017-01-25 22:04:27 -0800

    [feat] Finished routing for sockets

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 9000;
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const handleSocket = require('./sockets');
+
 //for passport middleware
 //app.use(express.session({ secret: 'keyboard cat' }));
 //app.use(passport.initialize());
@@ -15,15 +20,25 @@ app.use(express.static('client/build'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+	handleSocket(socket);
+	// socket.on('disconnect', () => {
+	// 	console.log('user has disconnected');
+	// });
+});
+
+
+http.listen(port, () => {
 	console.log("listening on port " + port);
 });
 
-module.exports.app = app;
 
 
 
