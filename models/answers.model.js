f5f5602ecee47dd74af2e3e5636b0ccced25d5da
Felipe Lee <fbslee@gmail.com>
2017-01-25 16:05:59 -0800

    [fix]schemas

---
+++ models/answers.model.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../db/config').sequelize;
+
+const Students = require('/students.model');
+
+const Question = require('/questions.model');
+
+const Answer = sequelize.define('answers', {
+  answerBody: Sequelize.STRING,
+  isCorrect: Sequelize.STRING,
+  answerTime: Sequelize.DATE
+});
+
+Students.hasMany(Answer);
+Question.hasMany(Answer);
+
+module.exports = Answer;
