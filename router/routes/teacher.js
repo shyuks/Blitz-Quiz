4673549478ab046c8206717a68264108e4c582ee
Bong Chu Chon <bongchon@Bongs-MacBook-Pro.local>
2017-01-28 15:04:21 -0800

    [feat]Fixed routings and front-end feat of login/signup

--- router/routes/teacher.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/test', (req, res) => {
-  console.log('Teacher route working!');
-  res.status(200).send('Teacher route is working!');
-});
-
-module.exports = router;
